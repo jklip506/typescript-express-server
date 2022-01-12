6f605da4d9c9b6728ee073ed6f4514ebd461fd98
Jason Klipple <jasonklipple@Jasons-MBP.lan>
2022-01-12 01:07:13 -0600

    01:06 12JAN2022

--- build/index.js
+++ build/index.js
@@ -4,14 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const loginRoutes_1 = require("./routes/loginRoutes");
+const body_parser_1 = __importDefault(require("body-parser"));
+const cookie_session_1 = __importDefault(require("cookie-session"));
 const app = (0, express_1.default)();
-app.get('/', (req, res) => {
-    res.send(`
-    <div>
-        <h1>Hi there</h1>
-    </div>
-    `);
-});
+//Adds body property
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.use((0, cookie_session_1.default)({ keys: ['asdlkasmdlkm'] }));
+app.use(loginRoutes_1.router);
 app.listen(3000, () => {
     console.log('listening port 3000');
 });
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
 import express, {Request, Response} from 'express';
+import { router } from './routes/loginRoutes';
+import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
 
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-    res.send(`
-    <div>
-        <h1>Hi there</h1>
-    </div>
-    `);
-});
-
+//Adds body property
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(cookieSession({ keys: ['asdlkasmdlkm']}));
+app.use(router);
 app.listen(3000, () => {
     console.log('listening port 3000')
-})
+});
